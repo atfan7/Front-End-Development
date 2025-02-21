5787cb784398b7464877ff29fef2073a5d273aa2
Mohd Atfan <143632074+atfan7@users.noreply.github.com>
2025-02-21 23:51:55 +0530

    Update script.js

--- Project-4-Custom_Designed_VideoPlayer/script.js
+++ Project-4-Custom_Designed_VideoPlayer/script.js
@@ -1,1 +1,67 @@
 
+const video =document.getElementById('video');
+const play =document.getElementById('play');
+const stop =document.getElementById('stop');
+const progress =document.getElementById('progress');
+const timestamp =document.getElementById('timestamp');
+
+
+//PLay and Pause of our Video
+
+function toggleVideoStatus(){
+    if(video.paused){
+        video.play();
+
+    } else {
+        video.pause();
+    }
+}
+
+//update the play and play icons
+function updatePlayIcon(){
+   if(video.paused){
+    play.innerHTML = '<i class="fa fa-play fa-2x"></i>';
+
+   }else {
+    play.innerHTML = '<i class="fa fa-pause fa-2x"></i>';
+   }
+}
+
+//update progress and timestamp
+
+function updateProgress(){
+    progress.value= (video.currentTime /video.duration) *100;
+
+
+}
+//set the timeline to progress
+
+function setVideoProgress(){
+    video.currentTime =(+progress.value *video.duration)/100
+}
+
+function stopVideo(){
+    video.currentTime=0;
+    video.pause();
+}
+
+
+
+//Event Listeners
+
+video.addEventListener('click',toggleVideoStatus);
+video.addEventListener('pause',updatePlayIcon);
+video.addEventListener('play',updatePlayIcon);
+video.addEventListener('timeupdate',updateProgress);
+
+
+
+play.addEventListener('click',toggleVideoStatus);
+
+stop.addEventListener('click',stopVideo);
+
+progress.addEventListener('change',setVideoProgress);
+
+
+
+
